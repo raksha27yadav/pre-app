8ec1023974f26f6ad97921b3e2b5f1d19536a7c4
Raksha Yadav <raksha.yadav@navyuginfo.com>
2025-09-14 10:44:47 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='mb-4'>
+      Hello World
+     </h1>
+     <p>Raksha Yadav</p>
+    </>
+  )
+}
+
+export default App
